e87dfb435194a2109765b0123142f702865da2eb
Jacob <jacob@Jacobs-MacBook-Pro.local>
2014-11-18 01:46:47 -0500

    updated so all tree methods are in tree and added unit testing

--- pix.py
+++ pix.py
@@ -1,5 +1,7 @@
 #! /usr/bin/python
 
+import unittest
+
 # checks if there is a combination of ropes in ropes
 # that equal n
 def ropes(n, ropes):
@@ -17,15 +19,6 @@ def ropes_helper(n, ropes):
         res = res + [sum(ropes[i:l-n])]
     return set(res)
 
-def test(n):
-    tlist = [2, 5, 6, 6]
-    ropes_test = lambda n: ropes(n, tlist)
-    for i in range(n):
-        print(str(i) + ": " + str(ropes_test(i)))
-
-print "DR. ROPES"
-test(20)
-print "\n"
 # ------------------------------------
 class Tree:
     def __init__(self, fruit, branch=[]):
@@ -35,29 +28,46 @@ class Tree:
     def __str__(self):
         return str(self.fruit)
 
-# returns if the tree is node
-def isNode(tree):
-    return len(tree.branch) == 0
+    # returns if the tree is node
+    def isNode(self):
+        return len(self.branch) == 0
 
-# returns maximum number of fruit on the given tree
-def max_fruit(tree):
-    if isNode(tree):
-        return tree.fruit
-    else:
-        return max(map(lambda x: (tree.fruit + max_fruit(x)), tree.branch))
+    # returns maximum number of fruit on the given tree
+    def max_fruit(self):
+        if self.isNode():
+            return self.fruit
+        else:
+            return max(map(lambda x: (self.fruit + x.max_fruit()), self.branch))
 
-# returns if the ant will be full if it goes down the given tree
-def ant_fed(n, tree):
-    return n >= max_fruit(tree)
+    # returns if the ant will be full if it goes down the given tree
+    def ant_fed(self, n):
+        return n >= self.max_fruit()
 
-l1 = Tree(5)
-l2 = Tree(10, [l1, l1])
-l3 = Tree(11, [l2, l1, l2])
-l4 = Tree(2, [l3, l2, l2, l1, l3])
+# ------------------------------------------------
 
-print "CURIOUS ANT"
+# testing class
+class TestAll(unittest.TestCase):
+    l1 = Tree(5)
+    l2 = Tree(10, [l1, l1])
+    l3 = Tree(11, [l2, l1, l2])
+    l4 = Tree(2, [l3, l2, l2, l1, l3])
 
-print not(ant_fed(3, l1));
-print ant_fed(6, l1);
-print not(ant_fed(12, l2));
-print ant_fed(30, l4);
+    def test_ant_fed(self):
+        self.assertTrue(not(self.l1.ant_fed(3)))
+        self.assertTrue(self.l1.ant_fed(6))
+        self.assertTrue(not(self.l2.ant_fed(12)))
+        self.assertTrue(self.l4.ant_fed(30))
+
+    def test_ropes(self):
+        tlist = [2, 5, 6, 6]
+        ropes_test = lambda n: ropes(n, tlist)
+        self.assertTrue(ropes_test(7))
+        self.assertTrue(ropes_test(11))
+        self.assertTrue(not(ropes_test(3)))
+        self.assertTrue(ropes_test(12))
+        self.assertTrue(ropes_test(17))
+        self.assertTrue(ropes_test(19))
+        self.assertTrue(not(ropes_test(15)))
+
+if __name__ == '__main__':
+    unittest.main()
